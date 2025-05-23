e5e76dfdfdd8ceaa6b18ba52529cb20ac88ae701
asdgradshow2023 <asdgradshow@gmail.com>
2025-05-22 20:50:19 -0400

    first half

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "475px",
+        "2xs": "380px",
+      },
+    },
+  },
+  plugins: [],
+};
